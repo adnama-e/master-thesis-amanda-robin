434b5e745afd7fcade41b211a21f516565b6520b
AmandaEliasson <dic13ael@student.lu.se>
2018-02-20 10:30:50 +0100

    gd

--- app/src/main/java/com/example/amandaeliasson/ecodrivning/MainActivity.java
+++ app/src/main/java/com/example/amandaeliasson/ecodrivning/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity /*implements NavigationView.
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+//hej
     }
 
     public ActionBarDrawerToggle setUpDrawerToggle() {
