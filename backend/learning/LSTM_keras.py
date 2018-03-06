d264fe13fd183ed60ced55bc91157a2cbd5925a7
Robin Timan <robin.timan@jayway.com>
2018-03-06 14:27:43 +0100

    Started implementation of RTA (real-time analysis) using a LSTM network for calculating an expected value. Please enter the commit message for your changes. Lines starting

--- backend/learning/LSTM_keras.py
+++ backend/learning/LSTM_keras.py
@@ -1,98 +1,83 @@
-import pandas as pd
-from utils import *
-import numpy as np
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM
 from math import sqrt
 from matplotlib import pyplot
 import argparse
-
-
-
-# The training data need to be transformed into a 3-D array [samples, timesteps, features]
-def filter_data(data, threshold=0.2):
-    correlations = data.corr(method="kendall")["Fuel_consumption"]
-    cols = list(data)
-    for i, corr in enumerate(correlations):
-        if np.isnan(corr) or np.abs(corr) < threshold:
-            if cols[i] != "Time(s)":
-                data = data.drop(cols[i], axis=1)
-
-    return data
-
-
-def scale_data(data):
-    scaler = MinMaxScaler((-1, 1))
-    scaled_values = scaler.fit_transform(data.as_matrix())
-    scaled_df = pd.DataFrame(scaled_values, columns=list(data))
-    return scaled_df, scaler
+from utils import *
+import pickle
 
 
 def build_model(data, settings):
-    y = data["Fuel_consumption"].as_matrix()
-    X = data.drop("Fuel_consumption", axis=1).as_matrix()
-    X = X.reshape(X.shape[0], 1, X.shape[1])
-    model = Sequential()
-    model.add(LSTM(settings["units"],
-                   batch_input_shape=(settings["batch_size"], X.shape[1], X.shape[2]), stateful=True))
-    model.add(Dense(1))
-    model.compile(loss="mean_squared_error", optimizer="adam")
-
-    for i in range(settings["num_epochs"]):
-        model.fit(X, y, epochs=1, batch_size=settings["batch_size"], verbose=1, shuffle=False)
-        model.reset_states()
-
-    return model
+	y = data["Fuel_consumption"].as_matrix()
+	X = data.drop("Fuel_consumption", axis=1).as_matrix()
+	X = X.reshape(X.shape[0], 1, X.shape[1])
+	model = Sequential()
+	model.add(LSTM(settings["units"],
+	               batch_input_shape=(settings["batch_size"], X.shape[1], X.shape[2]), stateful=True))
+	model.add(Dense(1))
+	model.compile(loss="mean_squared_error", optimizer="adam")
+	
+	for i in range(settings["num_epochs"]):
+		model.fit(X, y, epochs=1, batch_size=settings["batch_size"], verbose=1, shuffle=False)
+		model.reset_states()
+	
+	return model
 
 
 def predict_fuel_consumption(model, state, settings):
-    X = state.reshape(1, 1, state.size)
-    prediction = model.predict(X, batch_size=settings["batch_size"])
-    return prediction[0]
+	X = state.reshape(1, 1, state.size)
+	prediction = model.predict(X, batch_size=settings["batch_size"])
+	return prediction[0]
 
 
 def predict_test(model, data, settings):
-    # TODO investigate how to make it to multi-step predictions
-    num_states = data.shape[0]
-    predictions = []
-    for i in range(num_states):
-        state = data.iloc[[i]]
-        state_values = state.drop("Fuel_consumption", axis=1).as_matrix()
-        predicted_fuel = predict_fuel_consumption(model, state_values, settings)[0]
-        predictions.append(predicted_fuel)
-    return predictions
+	# TODO investigate how to make it to multi-step predictions
+	num_states = data.shape[0]
+	predictions = []
+	for i in range(num_states):
+		state = data.iloc[[i]]
+		state_values = state.drop("Fuel_consumption", axis=1).as_matrix()
+		predicted_fuel = predict_fuel_consumption(model, state_values, settings)[0]
+		predictions.append(predicted_fuel)
+	return predictions
 
 
 def evaluate(predictions, data, scaler):
-    for i, pred in enumerate(predictions):
-        state = data.iloc[[i]]
-        y_true = scaler.inverse_transform(state.as_matrix())
-        state["Fuel_consumption"] = pred
-        y_pred = scaler.inverse_transform(state.as_matrix())
-        rmse = sqrt(mean_squared_error(y_true, y_pred))
-        print("RMSE: {}, for state {}".format(rmse, i))
+	for i, pred in enumerate(predictions):
+		state = data.iloc[[i]]
+		y_true = scaler.inverse_transform(state.as_matrix())
+		state["Fuel_consumption"] = pred
+		y_pred = scaler.inverse_transform(state.as_matrix())
+		rmse = sqrt(mean_squared_error(y_true, y_pred))
+		print("RMSE: {}, for state {}".format(rmse, i))
 
 
 def plot_predictions(predictions, true_values, time_series):
-    pyplot.plot(time_series, predictions, label="Predicted")
-    pyplot.plot(time_series, true_values, 'r--', label="Actual")
-    pyplot.show()
+	pyplot.plot(time_series, predictions, label="Predicted")
+	pyplot.plot(time_series, true_values, 'r--', label="Actual")
+	pyplot.show()
 
-
-settings = {"batch_size": 1,
-            "lag": 1,
-            "units": 1,
-            "num_epochs": 30}
 
 
 # ###############################################
 # ###########          MAIN           ###########
 # ###############################################
 
-model_name = ""
-train_new_model = False
+settings = {"batch_size": 1,
+            "lag": 1,
+            "units": 1,
+            "num_epochs": 30}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--new-model", action="store_true", dest="train_new")
+parser.add_argument("--name", dest="model_name", default="lstm_model")
+parser.add_argument("-v", action="store_true", dest="visualize_it")
+args = parser.parse_args()
+
+model_name = args.model_name
+train_new_model = args.train_new
+visualize = args.visualize_it
 
 data = pd.read_csv("../datasets/KIA_driving_data.csv")
 data = data.drop("Class", axis=1)
@@ -103,24 +88,29 @@ train = train.drop("Time(s)", axis=1)
 train, scaler = scale_data(train)
 
 if train_new_model:
-    model = build_model(data, settings)
-    model.save(model_name)
+	print("Training new model..")
+	model = build_model(train, settings)
+	print("Model trained..")
+	model.save(model_name + ".h5")
+	pickle.dump(scaler, open(model_name + ".scl", "wb"))
+	pickle.dump(test_drives, open(model_name + ".test", "wb"))
 else:
-    model = load_model("lstm_model.h5")
+	model = load_model(model_name + ".h5")
 
-for drive in test_drives:
-    # This is just the timestamo, it shouldn't be part of the calculations,
-    time_series = drive["Time(s)"].values
-    drive = drive.drop("Time(s)", axis=1)
-    scaled_drive = scaler.transform(drive.as_matrix())
-    scaled_drive_df = pd.DataFrame(scaled_drive, columns=list(drive))
-    predictions = predict_test(model, scaled_drive_df, settings)
-
-    # Invert the transformation before plotting
-    true_fuel_consumption = drive["Fuel_consumption"].values
-    scaled_drive_df["Fuel_consumption"] = predictions
-    unscaled = scaler.inverse_transform(scaled_drive_df.as_matrix())
-    predicted_fuel_consumption = pd.DataFrame(unscaled, columns=list(drive))["Fuel_consumption"].values
-
-    plot_predictions(predicted_fuel_consumption, true_fuel_consumption, time_series)
+if visualize:
+	for drive in test_drives:
+		# This is just the timestamo, it shouldn't be part of the calculations,
+		time_series = drive["Time(s)"].values
+		drive = drive.drop("Time(s)", axis=1)
+		scaled_drive = scaler.transform(drive.as_matrix())
+		scaled_drive_df = pd.DataFrame(scaled_drive, columns=list(drive))
+		predictions = predict_test(model, scaled_drive_df, settings)
+		
+		# Invert the transformation before plotting
+		true_fuel_consumption = drive["Fuel_consumption"].values
+		scaled_drive_df["Fuel_consumption"] = predictions
+		unscaled = scaler.inverse_transform(scaled_drive_df.as_matrix())
+		predicted_fuel_consumption = pd.DataFrame(unscaled, columns=list(drive))["Fuel_consumption"].values
+		
+		plot_predictions(predicted_fuel_consumption, true_fuel_consumption, time_series)
 
--- backend/learning/utils.py
+++ backend/learning/utils.py
@@ -4,71 +4,118 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
 
+# Credit to: https://machinelearningmastery.com/convert-time-series-supervised-learning-problem-python/
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	"""
+	Frame a time series as a supervised learning dataset.
+	Arguments:
+		data: Sequence of observations as a list or NumPy array.
+		n_in: Number of lag observations as input (X).
+		n_out: Number of observations as output (y).
+		dropnan: Boolean whether or not to drop rows with NaN values.
+	Returns:
+		Pandas DataFrame of series framed for supervised learning.
+	"""
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = pd.DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = pd.concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+
+
 def scale_data(data):
-    scaler = MinMaxScaler((-1, 1))
-    scaled_values = scaler.fit_transform(data.as_matrix())
-    scaled_df = pd.DataFrame(scaled_values, columns=list(data))
-    return scaled_df, scaler
+	scaler = MinMaxScaler((-1, 1))
+	scaled_values = scaler.fit_transform(data.as_matrix())
+	scaled_df = pd.DataFrame(scaled_values, columns=list(data))
+	print("Data has been scaled to fit in the range -1 to 1")
+	return scaled_df, scaler
 
 
 def split_data(data, ratio=0.1, concat=True):
-    # TODO consider using K-fold validation
-    splitted_data = []
-    new_instance = False
-    start_index = 0
-    for index, time in enumerate(data["Time(s)"]):
-        if time == 1 and start_index != index:
-            new_instance = True
+	# TODO consider using K-fold validation
+	splitted_data = []
+	new_instance = False
+	start_index = 0
+	for index, time in enumerate(data["Time(s)"]):
+		if time == 1 and start_index != index:
+			new_instance = True
 
-        if new_instance and start_index != index:
-            splitted_data.append(data[start_index:index])
-            start_index = index
-            new_instance = False
+		if new_instance and start_index != index:
+			splitted_data.append(data[start_index:index])
+			start_index = index
+			new_instance = False
 
-    train_runs, test_runs = train_test_split(splitted_data, test_size=ratio)
-    if concat:
-        return pd.concat(train_runs), pd.concat(test_runs)
-    else:
-        return train_runs, test_runs
+	train_runs, test_runs = train_test_split(splitted_data, test_size=ratio)
+	if concat:
+		return pd.concat(train_runs), pd.concat(test_runs)
+	else:
+		return train_runs, test_runs
 
 
 def filter_data(data, threshold=0.2, filter_discrete=False):
-    # They're practically identical to Vehicle_speed.
-    always_drop = ["Wheel_velocity_front_left-hand", "Wheel_velocity_rear_right-hand",
-                   "Wheel_velocity_front_right-hand", "Wheel_velocity_rear_left-hand"]
-    data = data.drop(always_drop, axis=1)
-    correlations = data.corr(method="kendall")["Fuel_consumption"]
-    cols = list(data)
-    for i, corr in enumerate(correlations):
-        if filter_discrete:
-            col = data[cols[i]]
-            # print(cols[i], col.nunique() / col.count())
-            is_discrete = col.nunique() / col.count() < 0.001
-            if is_discrete:
-                data = data.drop(cols[i], axis=1)
-                continue
+	# They're practically identical to Vehicle_speed.
+	num_cols_before = len(list(data))
+	always_drop = ["Wheel_velocity_front_left-hand", "Wheel_velocity_rear_right-hand",
+				   "Wheel_velocity_front_right-hand", "Wheel_velocity_rear_left-hand"]
+	data = data.drop(always_drop, axis=1)
+	correlations = data.corr(method="kendall")["Fuel_consumption"]
+	cols = list(data)
+	for i, corr in enumerate(correlations):
+		if filter_discrete:
+			col = data[cols[i]]
+			# print(cols[i], col.nunique() / col.count())
+			is_discrete = col.nunique() / col.count() < 0.001
+			if is_discrete:
+				data = data.drop(cols[i], axis=1)
+				continue
 
-        if np.isnan(corr) or np.abs(corr) < threshold:
-            if cols[i] != "Time(s)":
-                data = data.drop(cols[i], axis=1)
-    return data
+		if np.isnan(corr) or np.abs(corr) < threshold:
+			if cols[i] != "Time(s)":
+				data = data.drop(cols[i], axis=1)
+				
+	num_cols_remaining = len(list(data))
+	print("{removed} columns filtered.\n"
+	      "{kept} columns remaining".format(removed=num_cols_before - num_cols_remaining,
+	                                        kept=num_cols_remaining))
+	return data
 
 
 def calc_road_limit(data, timestep, window_size=10):
-    """
-    This is done by taking the median vehicle speed of the last 10 time steps and the rounding off
-    to the nearest tenth.
-    We're assuming that the speed kept is reasonably withing the allowed limit.
-    :return: The speed
-    """
-    # TODO consider a more sophisticated way of doing this
-    steps_back = steps_forward = int(window_size / 2)
-    if timestep < steps_back:
-        steps_back = timestep
-        steps_forward = window_size - timestep
+	"""
+	This is done by taking the median vehicle speed of the last 10 time steps and the rounding off
+	to the nearest tenth.
+	We're assuming that the speed kept is reasonably withing the allowed limit.
+	:return: The speed
+	"""
+	# TODO consider a more sophisticated way of doing this
+	steps_back = steps_forward = int(window_size / 2)
+	if timestep < steps_back:
+		steps_back = timestep
+		steps_forward = window_size - timestep
 
-    vehicle_speed = data[timestep - steps_back: timestep + steps_forward].get("Vehicle_speed")
-    med = np.median(vehicle_speed.as_matrix())
-    return int(round(med/10)*10)
+	vehicle_speed = data[timestep - steps_back: timestep + steps_forward].get("Vehicle_speed")
+	med = np.median(vehicle_speed.as_matrix())
+	return int(round(med/10)*10)
 
+
+def load_data(filename):
+	data = pd.read_csv("../datasets/" + filename)
+	data = data.drop("Class", axis=1)
+	return data
 
